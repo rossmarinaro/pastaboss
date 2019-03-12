9e249580c86bcfb8efac123282953acb692bc1f9
Pastaboss <rossmarinaro@gmsil.com>
2019-03-11 20:16:10 -0700

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+/* javascript "Pastaboss"  */
+
+
+function toggle() {
+var x =
+document.getElementById("nav-bar");
+
+if (window.getComputedStyle(x,null).getPropertyValue("display") === 'none'){
+
+x.style.display = 'block'; }
+
+else {
+
+x.style.display = 'none'; }
+
+};
+
