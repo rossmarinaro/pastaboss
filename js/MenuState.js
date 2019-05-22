a09dee512250439d1188091a0b7fda575a350796
Ross <44373734+rossmarinaro@users.noreply.github.com>
2019-05-22 05:55:53 -0700

    Add files via upload

---
+++ js/MenuState.js
@@ -0,0 +1,53 @@
+//pastaboss game menu screen
+
+
+class MenuState extends Phaser.Scene{
+  constructor(){
+    super("MenuState");
+  }
+  create(){
+	  
+	//text
+	this.pastabossText = this.add.image(340, 130, 'pastaboss_text'); 
+	this.pastabossText.setScale(0.25);
+	this.pastabossText.setInteractive();
+	this.pastabossText.on('pointerdown', function(){
+	this.ring.play();
+	this.scene.start('PlayState');        
+	}, this);
+	this.startGameText = this.add.text(290, 190, "Start game", {font: "25px Bangers", fill: '#ffff00'}); 
+	this.startGameText.setInteractive();
+	this.startGameText.on('pointerdown', function(){
+	this.ring.play();
+	this.scene.start('PlayState');        
+	}, this);
+	this.add.text(75, 350, `use arrow keys, "W" and spacebar if on desktop, or on-screen controls for mobile.`, {font: "15px Arial", fill: "#ffff00"});
+	this.anims.create({
+	key: 'walk',
+	frames: this.anims.generateFrameNames('player', {prefix: 'p1_walk', start: 8, end: 3, zeroPad: 2}),
+	frameRate: 5,
+	repeat: 3
+    });
+	// start game button
+	player = this.add.sprite(350, 280, 'player').anims.play('walk');
+	player.setInteractive();
+	this.ring = this.sound.add('ring');
+	player.on('pointerdown', function(){
+	this.ring.play();
+	this.scene.start('PlayState');        
+	}, this);   
+	// menu audio
+	this.menuMusic = this.sound.add('menu_music');
+
+  }
+  
+ /*  update(){
+	  
+		 if (this){
+		  this.menuMusic.play();
+		 
+		 }
+	}
+*/	
+////////////
+}
